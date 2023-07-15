f2f67482d3a597f6e4265bbdb8889f2f0910fb93
Joe-appia <Joe-appia>
2023-07-15 07:16:50 -0700

    preprocessor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	printf("size of a char: %lu  byte(s)", sizeof(char));
-	printf("Size of an int: %lu  byte(s)", sizeof(int));
-	printf("Size of a long int:%lu  byte(s)", sizeof(long int));
-	printf("Size of a long long int: %lu  byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu  byte(s)", sizeof(float));
+	printf("size of a char: %lu 1 byte(s)", sizeof(char));
+	printf("Size of an int: %lu 4 byte(s)", sizeof(int));
+	printf("Size of a long int:%lu 4 byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu 8 byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu 4 byte(s)", sizeof(float));
 	return (0);
 
 }
