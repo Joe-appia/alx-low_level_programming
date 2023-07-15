9cf76713f2c06b2920bbfd09a51cbf76b3d98451
Joe-appia <Joe-appia>
2023-07-15 05:32:50 -0700

    preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * Hello c
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+ 
+	puts ("Programming is like building a multilingual puzzle");
 	return (0); 
 }
