d0e856383c8362cef09ca61126c99db404a2cb87
Joe-appia <Joe-appia>
2023-07-15 06:05:07 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print "with proper grammar, but the outcome is a piece of art," followed by newline
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
